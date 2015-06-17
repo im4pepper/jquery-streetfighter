6f3ef950e8a9a5a6c8b935fc54c2bed003c51465
im4pepper <jon@jonahlquist.com>
2015-06-16 22:28:05 -0600

    keyup and keydown events

--- js/app.js
+++ js/app.js
@@ -1,33 +1,43 @@
-$(document).ready(function(){
-    $('.ryu').mouseenter(function(){
-        $('.ryu-still').hide();
-        $('.ryu-ready').show();
-    })
-    .mouseleave(function() {
-        $('.ryu-still').show();
-        $('.ryu-ready').hide();
-    })
-    .mousedown(function(){
-        playHadouken();
-        $('.ryu-ready').hide();
-        $('.ryu-throwing').show();
-        $('.hadouken').finish().show().animate(
-            {'left': '1020px'},
-            500,
-            function(){
-                $(this).hide();
-                $(this).css('left', '520px');
-            });
-    })
-    .mouseup(function(){
-        $('.ryu-throwing').hide();
-        $('.ryu-ready').show();
-    });
-
-});
-
-function playHadouken() {
-        $('#hadouken-sound')[0].volume = 0.5;
-        $('#hadouken-sound')[0].load();
-        $('#hadouken-sound')[0].play();
+$(document).ready(function() {
+  $('.ryu').mouseenter(function() {
+    $('.ryu-still').hide();
+    $('.ryu-ready').show();
+  })
+  .mouseleave(function() {
+    $('.ryu-ready').hide();
+    $('.ryu-still').show();
+  })
+  .mousedown(function() {
+  	playHadouken();
+    $('.ryu-ready').hide();
+    $('.ryu-throwing').show();
+    $('.hadouken').finish().show().animate(
+  	  {'left': '1020px'},
+  	  400,
+  	  function() {
+    	$(this).hide();
+    	$(this).css('left', '520px');
+  	  }
+	);
+  })
+  .mouseup(function() {
+    $('.ryu-throwing').hide();
+  	$('.ryu-ready').show();
+  });
+  $(document).keydown(function(e) {
+    if (e.keyCode == 88) {
+      $('.ryu-action').hide();
+      $('.ryu-pose').show();
+    }   
+  }).keyup(function(e) {
+    if (e.keyCode == 88) {
+      $('.ryu-pose').hide();
+      $('.ryu-still').show();
     }
+  });
+});
+function playHadouken () {
+  $('#hadouken-sound')[0].volume = 0.5;
+  $('#hadouken-sound')[0].load();
+  $('#hadouken-sound')[0].play();
+}
